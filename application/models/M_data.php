02c44029079bb389d9d6247bd625b5723c3c6ec7
luisss29 <luisshu29@gmail.com>
2021-04-01 20:21:41 +0700

    Membuat fungsi validasi pada model M_data

--- application/models/M_data.php
+++ application/models/M_data.php
@@ -12,6 +12,23 @@ class M_data extends CI_Model{
                 $this->db->where('nim', $nim);
                 return $this->db->get('mahasiswa')->row();
         }
+
+        public function validation($mode)
+        {
+                $this->load->library('form_validation');
+                if($mode == "save")
+                        $this->form_validation->set_rules('input_nis', 'NIS', 'required|numeric|max_length[11]');
+                        
+                $this->form_validation->set_rules('input_nama', 'Nama', 'required|max_length[50]');
+                $this->form_validation->set_rules('input_jeniskelamin', 'Jenis Kelamin', 'required');
+                $this->form_validation->set_rules('input_telp', 'Telepon', 'required|numeric|max_length[15]');
+                $this->form_validation->set_rules('input_alamat', 'Alamat', 'required');
+                
+                if($this->form_validation->run())
+                        return TRUE;
+                else
+                        return FALSE;
+        }
 }
 
 ?>
